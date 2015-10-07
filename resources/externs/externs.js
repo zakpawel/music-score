7638ef6bdf10673d4355107a5c746d1f8d91c064
Pawel <pawel@MacBook-Pro-Pawel.local>
2015-10-07 12:58:53 +0200

    bumped clojurescript, externs for VexFlow

--- resources/externs/externs.js
+++ resources/externs/externs.js
@@ -1,7 +1,45 @@
 navigator.requestMIDIAccess = function(){};
 
-Instrument = {}
+var Instrument = {};
 Instrument.midiToPitch = function(){};
 
-ABCJS = {}
+var ABCJS = {};
 ABCJS.renderAbc = function(){};
+
+var Vex = {};
+Vex.Flow = {};
+Vex.Flow.StaveNote = function(){};
+Vex.Flow.StaveNote.addAccidental = function(){};
+Vex.Flow.StaveNote.setKeyStyle = function(){};
+
+
+Vex.Flow.Accidental = function(){};
+
+Vex.Flow.Voice = function(){};
+Vex.Flow.Voice.addTickables = function(){};
+
+Vex.Flow.RESOLUTION = {};
+
+Vex.Flow.Renderer = function(){};
+Vex.Flow.Renderer.Backends = {};
+Vex.Flow.Renderer.Backends.RAPHAEL = {};
+Vex.Flow.Renderer.Backends.CANVAS = {};
+
+
+Vex.Flow.Formatter = function(){};
+Vex.Flow.Formatter.format = function(){};
+Vex.Flow.Formatter.joinVoices = function(){};
+Vex.Flow.Formatter.draw = function(){};
+
+
+Vex.Flow.Stave = function(){};
+Vex.Flow.Stave.addClef = function(){};
+Vex.Flow.Stave.setContext = function(){};
+Vex.Flow.Stave.draw = function(){};
+
+Vex.Flow.TextNote = function(){};
+Vex.Flow.TextNote.Justification = {};
+Vex.Flow.TextNote.Justification.LEFT = {};
+Vex.Flow.TextNote.setStave = function(){};
+Vex.Flow.TextNote.setLine = function(){};
+Vex.Flow.TextNote.setJustification = function(){};
